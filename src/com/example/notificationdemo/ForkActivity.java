e52b289ec73eaf8a6c2714e47d4c258edc871cd8
zhangjian <278260927@qq.com>
2014-11-12 15:39:55 +0800

    add chong tu

--- src/com/example/notificationdemo/ForkActivity.java
+++ src/com/example/notificationdemo/ForkActivity.java
@@ -1,5 +1,12 @@
 package com.example.notificationdemo;
 
-public class ForkActivity {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class ForkActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		System.out.println("÷∆‘Ï≥ÂÕª");
+	}
 }

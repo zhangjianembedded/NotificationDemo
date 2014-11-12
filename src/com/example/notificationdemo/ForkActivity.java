23690ab8c217f9d481cb8cb5ba1d3b5657ce7829
zhangjian <278260927@qq.com>
2014-11-12 14:44:39 +0800

    Add onCreate in ForkActivity.

--- src/com/example/notificationdemo/ForkActivity.java
+++ src/com/example/notificationdemo/ForkActivity.java
@@ -1,5 +1,12 @@
 package com.example.notificationdemo;
 
-public class ForkActivity {
+import android.app.Activity;
+import android.os.Bundle;
 
+public class ForkActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		System.out.println("modify by zhangjian");
+		super.onCreate(savedInstanceState);
+	}
 }

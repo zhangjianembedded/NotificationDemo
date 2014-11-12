b4028ac5d1986959cae47261de919be3756d8998
zhangjian <278260927@qq.com>
2014-11-12 16:57:22 +0800

    第三个啦

--- src/com/example/notificationdemo/ThirdActivity.java
+++ src/com/example/notificationdemo/ThirdActivity.java
@@ -11,6 +11,7 @@ public class ThirdActivity extends Activity {
 		Toast.makeText(this, "解决BUG", Toast.LENGTH_LONG).show();
 		//表现的不错
 		Toast.makeText(this, "靠！又解决一个BUG", Toast.LENGTH_LONG).show();
+		Toast.makeText(this, "靠！又解决第三个BUG", Toast.LENGTH_LONG).show();
 	}
 
 }
